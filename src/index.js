9bd54007b7fab8b2e3b434f2d3392ed4cb22bab9
Hadi Debs <113387817+muffinman987@users.noreply.github.com>
2023-04-11 21:13:46 -0400

    all in one

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('starting up');
+// eslint-disable-next-line no-unused-vars
+import $ from 'jquery';
+// eslint-disable-next-line import/no-unresolved
+import './style.css';
+
+// $('#main').html(`Count: ${update()}`);
+
+let num = 0;
+
+const secondIncrease = () => {
+  const main = document.getElementById('main');
+  main.innerHTML = `You've been on this page for ${num} seconds`;
+  num += 1;
+};
+
+setInterval(secondIncrease, 1000);
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
+import eslint from 'vite-plugin-eslint';
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+
+import autoprefixer from 'autoprefixer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    eslint(),
+  ],
+
+  css: {
+    postcss: {
+      plugins: [autoprefixer(),
+      ],
+    },
+  },
+});
