7888f5325d172dbcc6b16942925ba5cf2455e312
Hadi Debs <113387817+muffinman987@users.noreply.github.com>
2023-04-27 22:52:11 -0400

    Fixed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Controls from './controls';
 
 function Counter(props) {
   const count = useSelector((store) => store.count);
   return (
     <div>
       Current Count: { count }
+      <Controls />
     </div>
   );
 }
