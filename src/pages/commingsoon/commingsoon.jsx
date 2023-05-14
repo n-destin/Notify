ff2fc2a69bde638f5a10778dfea4c5abd9bce813
Judson Dunne <41300381+Judderbudder@users.noreply.github.com>
2023-05-14 04:02:19 -0400

    Commited for destin :-)

---
+++ src/pages/commingsoon/commingsoon.jsx
@@ -0,0 +1,13 @@
+import './commingsoon.scss';
+import React from 'react';
+
+function Comingsoon() {
+    return (
+        <div className="comingsoon">
+            Coming soon
+
+        </div>
+    );
+}
+
+export default Comingsoon;
