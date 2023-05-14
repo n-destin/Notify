ce76e513849ba6e418c76d74456366c63b7cb710
Judson Dunne <41300381+Judderbudder@users.noreply.github.com>
2023-05-14 03:32:53 -0400

    Worked on the chat page

---
+++ src/pages/chat_page/chat_page.jsx
@@ -0,0 +1,111 @@
+import './chat_page.scss';
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TextField } from '@mui/material';
+
+function Chatpage() {
+    return (
+        <div className="chatpage">
+            <div className="window">
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        Hello, how are you doing
+
+                    </div>
+                    <img src="" alt="user-profile" />
+                </div>
+                <div className="chat-container">
+                    <div className="chat">
+                        I am good!
+
+                    </div>
+                    <img src="" alt="ai-profile" />
+                </div>
+
+            </div>
+            <div className="input">
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+
+            </div>
+        </div>
+    );
+}
+
+export default Chatpage;
