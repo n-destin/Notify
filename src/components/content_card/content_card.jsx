dd71916b72d63b21163d9eef5cc156e4ddaad615
Judson Dunne <41300381+Judderbudder@users.noreply.github.com>
2023-05-14 02:46:01 -0400

    Added more to "home page" including adding photo and caption

--- src/components/content_card/content_card.jsx
+++ src/components/content_card/content_card.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
 import './content_card.scss';
 import React from 'react';
 
 function Contentcard() {
-    return (
-        <div>Contentcard</div>
-    );
+  return (
+    <div className="contentcard">
+      <div className="image-container">
+        <img src="src/assets/3d-icon-file-page-document-with-magnifying-search-information-data-analysis_22052-4082 1.png" alt="" />
+      </div>
+      <div className="caption">
+        <span>
+          Lecture #0
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Contentcard;
