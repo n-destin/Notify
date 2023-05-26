b3e4629eb49e491f7d7b6f297374a1baf6818de4
CancheMichael <michael.i.canche.23@dartmouth.edu>
2023-05-25 20:13:45 -0400

    Testing commits

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,15 +1,15 @@
-import * as Firebase from '../services/firebase'
-import axios from 'axios'
-import { useEffect } from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import * as Firebase from '../services/firebase';
 import { actions } from '../reducers/postsreducer';
 
-export function getPost(id){
-    return (dispatch)=>{
-        useEffect(Firebase.onSessionChange(id, (Lecture)=>{
+export function getPost(id) {
+    return (dispatch) => {
+        useEffect(Firebase.onSessionChange(id, (Lecture) => {
             dispatch({
-                type : actions.getLecture,
-                payload: Lecture
+                type: actions.getLecture,
+                payload: Lecture,
             });
         }), []);
-    }
+    };
 }
