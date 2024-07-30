6c1fbe74073c598148c3f50ee3cdb1c21030d1f1
Destin Niyomufasha <destin.niyomufasha.26@dartmouth.edu>
2024-07-30 02:08:35 -0400

    changes

--- src/pages/chat_page/ChatPage.jsx
+++ src/pages/chat_page/ChatPage.jsx
@@ -169,7 +169,7 @@ function ServerChatMessage({
     }
 
     async function streamContent(serverChatId) {
-        const API_KEY = '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'; // need to make ENV var
+        const API_KEY = ''; // need to make ENV var
         const url = 'https://api.openai.com/v1/chat/completions';
 
         const data = {
